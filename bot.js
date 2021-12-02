045c28f143f4c09ba8d5bc28224839fa075a455a
sssaaaaaammm <saveliymaslakov969@gmail.com>
2021-12-02 22:48:54 +0700

    bot vers.1

--- bot.js
+++ bot.js
@@ -2,8 +2,17 @@ require('dotenv').config();
 const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply('Welcome'));
+bot.start((ctx) => {
+  if (ctx.chat.username === 'eepppc') {
+    ctx.reply('Welcome back, boss');
+  } else {
+    ctx.reply(`Hello, ${ctx.message.from.first_name}`);
+  }
+});
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.hears('hi', (ctx) => ctx.reply('Wassup'));
+bot.hears('inf', (ctx) => ctx.reply(ctx.chat));
+bot.on('message', (ctx) => ctx.reply('error, try again later'));
+
 bot.launch();
